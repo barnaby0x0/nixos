4ad5ec311a208ffb71ba1f2e35fa37382e541243
Victor <victor@mail.net>
2025-08-14 11:55:32 +0200

    Working on refactoring #7

--- hosts/commons/users/user/default.nix
+++ hosts/commons/users/user/default.nix
@@ -12,7 +12,6 @@
       extraGroups     = [ "users" "wheel" ];
       group = "user";
       home = "/home/user";
-      useDefaultShell = true;
       hashedPassword = "$6$FdYq/J2XbivlPLEG$Jx7ppF65H0vj/.6/118K/d1LUFa4dyucTSk9KTSxlDhIHGH/puUDIIGNx5fcn5H7XgTHPPa.TyyaIugiLw6GF/";
       isNormalUser = true;
       shell = pkgs.zsh;
