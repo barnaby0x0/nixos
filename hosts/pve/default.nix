b7fea80ffa90cb7718220391df645d4cbd3f74ec
Victor <victor@mail.com>
2025-08-22 17:13:43 +0200

    Add pve host (for testing purpose), fix6

--- hosts/pve/default.nix
+++ hosts/pve/default.nix
@@ -7,7 +7,7 @@
       ./hardware-builder.nix
       ./bootloader.nix
       ./custom-configuration.nix
-      ./users.nix
+      # ./users.nix
       ../commons/users/user
     ];
 
@@ -67,16 +67,16 @@
   #users.mutableUsers = false;
   #users.users.vagrant.shell = pkgs.zsh;
 
-  security.sudo.extraConfig =
-    ''
-      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
-      Defaults:root,%wheel env_keep+=NIX_PATH
-      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
-      Defaults env_keep+=SSH_AUTH_SOCK
-      Defaults lecture = never
-      root   ALL=(ALL) SETENV: ALL
-      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
-    '';
+  # security.sudo.extraConfig =
+  #   ''
+  #     Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+  #     Defaults:root,%wheel env_keep+=NIX_PATH
+  #     Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+  #     Defaults env_keep+=SSH_AUTH_SOCK
+  #     Defaults lecture = never
+  #     root   ALL=(ALL) SETENV: ALL
+  #     %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+  #   '';
 
 }
 
