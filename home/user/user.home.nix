1cd5f3898e52a3023e94353ad8bd456155b5bf46
Victor <victor@mail.com>
2026-01-06 15:57:02 +0100

    Upgrade vim plugin manager sha256 plug

--- home/user/user.home.nix
+++ home/user/user.home.nix
@@ -22,7 +22,7 @@
 
   home.file.".vim/autoload/plug.vim".source = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
-    sha256 = "sha256-wtiZhGmgSaUSJacRKKEpF7N5gi0WtjlJPinqAth4cwY=";
+    sha256 = "sha256-4JmeVzBIZedfWxXuhjfcTOW6lZF1V/OPfLY9RUtTz7Q=";
   };
 
   # Gestionnaire de shell et configuration de l’environnement
